51884a1a346065cde5e75b7c0a5e07a27e7009fe
ANA LUIZA COSTA LIMA <FR042147@catolica.ubec>
2020-01-29 21:39:06 -0300

    add jest

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ app.get("/", (req, res) => {
     res.send("Oiee!");
 });
 
+module.exports = app
+
 app.listen(3000, () => {
     console.log("Servidor funcionando em http://localhost:3000!");
 });
+
+
 
